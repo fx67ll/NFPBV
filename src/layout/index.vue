2ef2992f5cf5700b6f9c74774e8bb089246ab707
fx67ll <2717745014@qq.com>
2021-08-05 14:53:58 +0800

    修改示例菜单的名称

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -61,10 +61,10 @@
 			}"
 		>
 			<el-menu v-if="!isAside" class="fx67ll-layout-header" mode="horizontal" @select="handleSelect">
-				<el-menu-item index="1">处理中心</el-menu-item>
+				<el-menu-item index="1">导航一</el-menu-item>
 				<el-submenu index="2">
 					<template slot="title">
-						我的工作台
+						导航二
 					</template>
 					<el-menu-item index="2-1">选项1</el-menu-item>
 					<el-menu-item index="2-2">选项2</el-menu-item>
@@ -78,8 +78,8 @@
 						<el-menu-item index="2-4-3">选项3</el-menu-item>
 					</el-submenu>
 				</el-submenu>
-				<el-menu-item index="3" disabled>消息中心</el-menu-item>
-				<el-menu-item index="4"><a href="https://www.fx67ll.com" target="_blank">订单管理</a></el-menu-item>
+				<el-menu-item index="3" disabled>导航三</el-menu-item>
+				<el-menu-item index="4"><a href="https://www.fx67ll.com" target="_blank">导航四</a></el-menu-item>
 			</el-menu>
 			<div
 				class="fx67ll-layout-view"
