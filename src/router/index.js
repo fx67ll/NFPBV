fb9e4b404a29093efd77649a928c6b103be24f0d
fx67ll <2717745014@qq.com>
2023-03-16 19:28:48 +0800

    use history mode

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ export const fx67llRoutes = [{
 }]
 
 const router = new Router({
-	mode: 'hash', // history模式，去掉url中的#
+	mode: 'history', // history模式，去掉url中的#
 	scrollBehavior: () => ({
 		x: 0,
 		y: 0
