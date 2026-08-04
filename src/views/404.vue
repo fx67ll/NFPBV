9ec8b07d6695dfd637f08ef209dbd9f19ff8190e
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-08-04 11:43:13 +0800

    optimize 404 error page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="fx67ll-404">
 		<nav class="shelf">
-			<a class="book home-page" href="https://fx67ll.com" target="_blank">fx67ll.com</a>
-			<a class="book about-us" href="https://fx67ll.xyz" target="_blank">fx67ll.xyz</a>
-			<a class="book contact" href="https://github.com/fx67ll" target="_blank">F.A.Q.</a>
-			<a class="book faq" href="#" @click.prevent="back()">返回上一页</a>
+			<a class="book home-page" href="https://fx67ll.com" target="_blank">个人主页</a>
+			<a class="book contact" href="https://fx67ll.xyz/s/messageboard" target="_blank">联系管理员</a>
+			<a class="book about-us" href="https://fx67ll.xyz" target="_blank">技术博客</a>
+			<a class="book faq" href="" @click="back()">返回上一页</a>
 			<span class="book not-found"></span>
 			<span class="door left"></span>
 			<span class="door right"></span>
 		</nav>
-		<h1>Error 404</h1>
-		<p>糟糕，您要找的网页消失了，请联系管理员。。。</p>
+		<h1>404</h1>
+		<p>糟糕，当前网页不存在，如有需要请留言咨询~</p>
 		<div class="fx67ll-footer">
 			Designed & Powered by
-			<a href="https://fx67ll.com" target="_blank">fx67ll</a>
+			<a href="https://nav.fx67ll.com" target="_blank">fx67ll</a>
 			&#12288; Copyright© 2018-{{ year }} &#12288;
 			<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">皖ICP备18017174号</a>
 		</div>
@@ -31,7 +31,6 @@ export default {
 	},
 	methods: {
 		back() {
-			console.log(window.history);
 			if (window.history.length <= 1) {
 				this.$router.push({ path: '/' });
 				return false;
@@ -45,6 +44,7 @@ export default {
 
 <style lang="less" scoped="scoped">
 @import '@a/styles/404.less';
+
 .fx67ll-footer {
 	width: 100%;
 	padding: 20px 0;
